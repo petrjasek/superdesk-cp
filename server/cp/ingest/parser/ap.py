a98da3501f71f1edfca8cc180eba619a4fbefe03
Petr Jasek <jasekpetr@gmail.com>
2020-04-08 16:28:33 +0200

    save

--- server/cp/ingest/parser/ap.py
+++ server/cp/ingest/parser/ap.py
@@ -1,7 +1,12 @@
 
 import re
+import json
+import tempfile
 
+from superdesk.datalayer import SuperdeskJSONEncoder
 from superdesk.io.feed_parsers import APMediaFeedParser
+
+from cp.orangelogic import OrangelogicSearchProvider
 
 
 class CP_APMediaFeedParser(APMediaFeedParser):
@@ -9,8 +14,29 @@ class CP_APMediaFeedParser(APMediaFeedParser):
     def process_slugline(self, slugline):
         return re.sub(r'--+', '-', re.sub(r'[ !"#$%&()*+,./:;<=>?@[\]^_`{|}~\\]', '-', slugline))
 
-    def parse(self, s_json, provider=None):
-        item = super().parse(s_json, provider=provider)
+    def parse(self, data, provider=None):
+        item = super().parse(data, provider=provider)
         if item.get('slugline'):
             item['slugline'] = self.process_slugline(item['slugline'])
+
+        with open('/tmp/{}.json'.format(item['slugline']), mode='wt') as f:
+            json.dump(data, f, indent=2, default=SuperdeskJSONEncoder().default)
+            print('saved', f.name)
+
+        self.parse_associations(data, item)
+
         return item
+
+    def parse_associations(self, data, item):
+        try:
+            assoc = data['data']['item']['associations']
+        except KeyError:
+            return
+
+        item['associations'] = {}
+        orange = OrangelogicSearchProvider({'config': {}})
+
+        for key, val in assoc.items():
+            guid = val['altids']['itemid']
+            related = orange.fetch(guid)
+            item['associations'][key] = related
--- server/tests/ingest/parser/ap_test.py
+++ server/tests/ingest/parser/ap_test.py
@@ -1,14 +1,31 @@
 
+import json
+import flask
 import unittest
 
+from unittest.mock import MagicMock
+from tests.ingest.parser import get_fixture_path
 
 from cp.ingest import CP_APMediaFeedParser
 
 
+with open(get_fixture_path('item.json', 'ap')) as fp:
+    data = json.load(fp)
+
+
 class CP_AP_ParseTestCase(unittest.TestCase):
+
+    app = flask.Flask(__name__)
 
     def test_slugline(self):
         parser = CP_APMediaFeedParser()
         self.assertEqual('foo-bar-baz', parser.process_slugline('foo bar/baz'))
         self.assertEqual('foo-bar', parser.process_slugline('foo-bar'))
         self.assertEqual('foo-bar', parser.process_slugline('foo - bar'))
+
+    def test_parse_associations(self):
+        parser = CP_APMediaFeedParser()
+        item = {}
+        with self.app.app_context():
+            parser.parse_associations(data, item)
+        self.assertIn('associations', item)
